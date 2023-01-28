58ea1f7b0021e6079a45c1e5a5c16e94e41a095e
Sean Brady <sbrady@efilecabinet.com>
2023-01-28 11:14:51 -0700

    test calling twitter api

--- project_lab_1.py
+++ project_lab_1.py
@@ -10,6 +10,10 @@ def get_auth_header():
   bearer_token = Variable.get("TWITTER_BEARER_TOKEN", deserialize_json=True)
   return {"Authorization": f"Bearer {bearer_token}"}
 
+def send_request(api_url):
+  request = requests.get(api_url, headers=get_auth_header())
+  return request.json()
+
 # user_id = "44196397"
 # api_url = f"https://api.twitter.com/2/users/{user_id}"
 # request = requests.get(api_url, headers=get_auth_header())
@@ -26,14 +30,23 @@ def my_task_func_2(ti: TaskInstance, **kwargs):
   return
   #Should log the list [1,2,3,4,5] to this task's log.
 
-# Calls the twitter api using the python requests module
-# Pulls your bearer token and creates an authentication header (like in the requests script tutorial)
-# Runs a request for every single user-id and every single tweet-id.
-# The user request should pull the following information: public_metrics, profile_image_url,username,description,and the id.
-# The tweet request should pull the following information: public_metrics, author_id, and the tweet text.
-# Once the information has been collected, push it to the next task to be transformed into a readable format. I’d recommend pushing two separate lists, the user_requests, and the tweet_requests.
-def get_twitter_api_data_task(ti: TaskInstance, **kwargs):
-    0
+def load_tweets(ids):
+  return send_request(f"https://api.twitter.com/2/tweets?ids={','.join(ids)}&tweet.fields=public_metrics,author_id,text")
+
+def load_users(ids):
+  return send_request(f"https://api.twitter.com/2/users?ids={','.join(ids)}&user.fields=public_metrics,profile_image_url,username,description,id")
+
+def get_twitter_api_data(ti: TaskInstance, **kwargs):
+  tweet_ids = Variable.get("TWITTER_TWEET_IDS", deserialize_json=True)
+  tweets = load_tweets(tweet_ids)
+  log.info(tweets)
+  ti.xcom_push("tweets", tweets)
+  user_ids = Variable.get("TWITTER_USER_IDS", deserialize_json=True)
+  users = load_users(user_ids)
+  log.info(users)
+  ti.xcom_push("users", users)
+  return
+  #Should log the list [1,2,3,4,5] to this task's log.
 
 with DAG(
     dag_id="project_lab_1_etl",
@@ -51,5 +64,10 @@ with DAG(
         python_callable=my_task_func_2,
         provide_context=True,
     )
+    get_twitter_api_data_task = PythonOperator(
+        task_id="get_twitter_api_data",
+        python_callable=get_twitter_api_data,
+        provide_context=True
+    )
 
-my_dummy_task >> my_dummy_task_two
+my_dummy_task >> my_dummy_task_two >> get_twitter_api_data_task
