62e2bb6eaceebe0f21e464967b0f7b9a8e39d212
Sean Brady <sbrady@efilecabinet.com>
2023-01-30 09:03:54 -0700

    try 1

--- project_lab_1.py
+++ project_lab_1.py
@@ -1,3 +1,6 @@
+import sys; 
+import os;
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from airflow import DAG
 from airflow.models import Variable
 from airflow.models import TaskInstance
