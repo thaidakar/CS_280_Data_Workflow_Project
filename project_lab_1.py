76ebab83e51aaf214a1f45c1beec1f262e0c44d4
Sean Brady <sbrady@efilecabinet.com>
2023-01-30 08:40:07 -0700

    databox integration

--- project_lab_1.py
+++ project_lab_1.py
@@ -7,6 +7,7 @@ from airflow.operators.python import PythonOperator
 import requests
 import pandas as pd
 from google.cloud import storage
+from databox import Client
 
 def get_auth_header():
   log.info("getting token")
@@ -65,6 +66,30 @@ def transform_twitter_api_data_func(ti: TaskInstance, **kwargs):
   bucket.blob("data/tweets.csv").upload_from_string(tweets_df.to_csv(index=False), "text/csv")
   bucket.blob("data/users.csv").upload_from_string(users_df.to_csv(index=False), "text/csv")
 
+def push_data_to_databox(name, metric: int):
+  client_token = Variable.get("DATABOX_TOKEN")
+  client = Client(f"{client_token}")
+  client.push(name, metric)
+
+def upload_databox_data(ti: TaskInstance, **kwargs):
+  users = pd.read_csv('gs://s-b-apache-airflow-cs280/data/users.csv')
+
+  for user in users:
+    name = user['name']
+    push_data_to_databox(f'{name}_followers_count', user['followers_count'])
+    push_data_to_databox(f'{name}_following_count', user['following_count'])
+    push_data_to_databox(f'{name}_tweet_count', user['tweet_count'])
+    push_data_to_databox(f'{name}_listed_count', user['listed_count'])
+
+  tweets = pd.read_csv('gs://s-b-apache-airflow-cs280/data/tweets.csv')
+
+  for tweet in tweets:
+    id = tweet['tweet_id']
+    push_data_to_databox(f'{id}', tweet['reply_count'])
+    push_data_to_databox(f'{id}', tweet['like_count'])
+    push_data_to_databox(f'{id}', tweet['impression_count'])
+    push_data_to_databox(f'{id}', tweet['retweet_count'])
+
 with DAG(
     dag_id="project_lab_1_etl",
     schedule_interval="0 9 * * *",
@@ -91,5 +116,10 @@ with DAG(
       python_callable=transform_twitter_api_data_func,
       provide_context=True
     )
+    upload_databox_data_task = PythonOperator(
+      task_id="upload_databox_data",
+      python_callable=upload_databox_data,
+      provide_context=True
+    )
 
-my_dummy_task >> my_dummy_task_two >> get_twitter_api_data_task >> transform_twitter_api_data_task
+my_dummy_task >> my_dummy_task_two >> get_twitter_api_data_task >> transform_twitter_api_data_task >> upload_databox_data_task
