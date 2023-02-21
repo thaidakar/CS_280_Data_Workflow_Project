3e098100b73f5cd241acfb669a1d6981e43f0f70
Sean Brady <sbrady@efilecabinet.com>
2023-02-21 12:34:18 -0700

    more import

--- models/TweetTimeseries.py
+++ models/TweetTimeseries.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date, Float
 
 Base = declarative_base()
